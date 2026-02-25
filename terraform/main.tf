b0c03f6edd363fb6770055526312edd5d8c59ff9
Adisitorus123 <adimansitorus12@gmail.com>
2026-02-26 00:01:48 +0700

    create github/workflows & update terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,6 @@ resource "google_compute_network" "vpc" {
   name = "app-vpc"
 }
 
-# Subnet Public
 resource "google_compute_subnetwork" "public" {
   name          = "public-subnet"
   ip_cidr_range = "10.0.1.0/24"
@@ -10,7 +9,6 @@ resource "google_compute_subnetwork" "public" {
   network       = google_compute_network.vpc.id
 }
 
-# Subnet Private
 resource "google_compute_subnetwork" "private" {
   name          = "private-subnet"
   ip_cidr_range = "10.0.2.0/24"
@@ -18,7 +16,6 @@ resource "google_compute_subnetwork" "private" {
   network       = google_compute_network.vpc.id
 }
 
-# Firewall untuk frontend
 resource "google_compute_firewall" "allow_http" {
   name    = "allow-http"
   network = google_compute_network.vpc.name
@@ -31,7 +28,6 @@ resource "google_compute_firewall" "allow_http" {
   source_ranges = ["0.0.0.0/0"]
 }
 
-# Frontend VM (public)
 resource "google_compute_instance" "frontend" {
   name         = "frontend-vm"
   machine_type = "e2-micro"
@@ -55,7 +51,6 @@ resource "google_compute_instance" "frontend" {
   EOT
 }
 
-# Backend VM (private)
 resource "google_compute_instance" "backend" {
   name         = "backend-vm"
   machine_type = "e2-micro"
@@ -78,7 +73,20 @@ resource "google_compute_instance" "backend" {
   EOT
 }
 
-# Cloud SQL (Postgres)
+resource "google_compute_global_address" "private_range" {
+  name          = "private-ip-range"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 16
+  network       = google_compute_network.vpc.self_link
+}
+
+resource "google_service_networking_connection" "private_vpc_connection" {
+  network                 = google_compute_network.vpc.self_link
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_range.name]
+}
+
 resource "google_sql_database_instance" "db" {
   name             = "app-db"
   database_version = "POSTGRES_14"
@@ -90,6 +98,7 @@ resource "google_sql_database_instance" "db" {
       private_network = google_compute_network.vpc.self_link
     }
   }
+  depends_on = [google_service_networking_connection.private_vpc_connection]
 }
 
 resource "google_sql_user" "users" {
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,31 @@
 provider "google" {
-  credentials = file("key.json")
+  credentials = file("${path.module}/../key.json")
   project     = var.project_id
   region      = var.region
   zone        = var.zone
+}
+
+resource "google_project_service" "compute_api" {
+  project = var.project_id
+  service = "compute.googleapis.com"
+}
+
+resource "google_project_service" "sql_api" {
+  project = var.project_id
+  service = "sqladmin.googleapis.com"
+}
+
+resource "google_project_service" "cloud_resource_manager" {
+  project = var.project_id
+  service = "cloudresourcemanager.googleapis.com"
+}
+
+resource "google_project_service" "service_networking" {
+  project = var.project_id
+  service = "servicenetworking.googleapis.com"
+}
+
+resource "google_project_service" "artifact_api" {
+  project = var.project_id
+  service = "artifactregistry.googleapis.com"
 }
